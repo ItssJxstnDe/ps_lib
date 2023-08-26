909eb4e6badf11fa8bd66b60a4c29c3fb434f2c0
ItssJxstnDe <127620206+ItssJxstnDe@users.noreply.github.com>
2023-08-26 11:25:50 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -273,18 +273,17 @@ GithubUpdater = function()
     GetCurrentVersion = function()
 	    return GetResourceMetadata( GetCurrentResourceName(), "version" )
     end
-    
     local CurrentVersion = GetCurrentVersion()
     local resourceName = "^4["..GetCurrentResourceName().."]^0"
 
     if ps.VersionChecker then
-        PerformHttpRequest('https://raw.githubusercontent.com/MSK-Scripts/msk_core/main/VERSION', function(Error, NewestVersion, Header)
+        PerformHttpRequest('https://raw.githubusercontent.com/ItssJxstnDe/ps_lib/main/VERSION', function(Error, NewestVersion, Header)
             print("###############################")
             if CurrentVersion == NewestVersion then
                 print(resourceName .. '^2 ✓ Resource is Up to Date^0 - ^5Current Version: ^2' .. CurrentVersion .. '^0')
             elseif CurrentVersion ~= NewestVersion then
                 print(resourceName .. '^1 ✗ Resource Outdated. Please Update!^0 - ^5Current Version: ^1' .. CurrentVersion .. '^0')
-                print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here:^9 https://github.com/MSK-Scripts/msk_core/releases/tag/v'.. NewestVersion .. '^0')
+                print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here:^9 https://github.com/ItssJxstnDe/ps_lib/releases/tag/v'.. NewestVersion .. '^0')
             end
             print("###############################")
         end)
